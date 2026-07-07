6907b998f6ed25a6d0d710ad056c90dc763a74fe
OscarVlc <emanuel.velasco@iteso.mx>
2026-07-07 09:46:57 -0600

    hola

---
+++ app.js
@@ -0,0 +1,12 @@
+const momment =  require('moment');
+
+let i = 5;
+let j = 10;
+
+console.log("Iniciando...");
+console.log('La suma es: '+ (5+10));
+
+let fecha =momment().format("LL");
+console.log('Y la fecha es: ', fecha);
+
+
